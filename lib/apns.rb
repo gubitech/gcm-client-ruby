1830d8707922ad9e34911b3a5fde214b3593208f
gubitech <gubitech2011@gmail.com>
2014-07-11 12:02:17 +0100

    fix requires

--- lib/apns.rb
+++
@@ -1,9 +0,0 @@
-require 'apns/client'
-require 'apns/notification'
-require 'apns/notification_response'
-require 'apns/response'
-require 'apns/version'
-
-module APNS
-  class Error < StandardError; end
-end
---
+++ lib/gcm.rb
@@ -0,0 +1,9 @@
+require 'gcm/client'
+require 'gcm/notification'
+require 'gcm/notification_response'
+require 'gcm/response'
+require 'gcm/version'
+
+module GCM
+  class Error < StandardError; end
+end
